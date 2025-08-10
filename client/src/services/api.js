3d4785ce8cc5e3c7623f40427a63801aff96f90a
Anjali <148552134+AnjaliCSE03@users.noreply.github.com>
2025-08-10 22:20:31 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "import axios from "axios";
+
+const API_URL = "https://job-list-application.onrender.com";
+
+export const saveData = async (data) => {
+
+    try{
+        await axios.post(`${API_URL}/saveData`, data);
+    }catch(error){
+        console.error('Error while calling save data api', error.message);
+    }
+    
+}
+
+export const getData = async (data) => {
+
+    try{
+        let response = await axios.get(`${API_URL}/get`);
+        return response.data;
+    }catch(error){
+        console.error('Error while calling save data api', error.message);
+    }
+    
+}";
 
 export const saveData = async (data) => {
 
