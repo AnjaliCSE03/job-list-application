c44acda4c0e98791d6233dd5dec32e542a2cf828
AnjaliCSE03 <anjali.14ldh@gmail.com>
2025-08-10 20:22:39 +0530

    Initial commit

---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const dbConnection = async() => {
+
+    const DB_URI = "mongodb+srv://anjali14ldh:7byDC0EhjWtUftdy@job-portal.tsf919k.mongodb.net/";
+
+    try{
+        await mongoose.connect(DB_URI, { useUnifiedTopology: true});
+        console.log('Database connected successfully.');
+    }
+    catch(error){
+        console.log('Error while connecting with the database.', error.message);
+    }
+
+}
+
+export default dbConnection;
