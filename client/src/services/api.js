59bebf32058e4fa4a18798d21a0a18527ecda5d7
AnjaliCSE03 <anjali.14ldh@gmail.com>
2025-08-10 20:28:25 +0530

    Initial commit - full project with client and server

---
+++ client/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000";
+
+export const saveData = async (data) => {
+
+    try{
+        await axios.post(`${API_URL}/saveData`, data);
+    }catch(error){
+        console.error('Error while calling save data api', error.message);
+    }
+    
+}
+
+export const getData = async (data) => {
+
+    try{
+        let response = await axios.get(`${API_URL}/get`);
+        return response.data;
+    }catch(error){
+        console.error('Error while calling save data api', error.message);
+    }
+    
+}
